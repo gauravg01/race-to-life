0532a7e7e148d9bc1f75fa72c539ed1b65aa9fa6
nrakeshkumar123 <nrakeshkumar123@gmail.com>
2022-04-11 21:21:49 -0400

    Adding Character Creation and NPC scripts

---
+++ src/ChacterController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class ChacterController : MonoBehaviour
+{
+    public CinemachineVirtualCamera cinemachineVirtual;
+    public GameObject[] camerarroot;
+    public GameObject[] players;
+    private void Awake()
+    {
+        cinemachineVirtual.Follow = camerarroot[PlayerPrefs.GetInt("char")].transform;
+        players[PlayerPrefs.GetInt("char")].SetActive(true);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKey(KeyCode.Backspace))
+        //{
+        //    UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        //}
+    }
+    public void back()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
